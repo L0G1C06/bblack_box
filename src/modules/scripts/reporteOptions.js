655c680e154b68fe8f068c467a34abe92ecf8119
L0G1C06 <justgithub70@gmail.com>
2025-04-09 16:37:06 -0300

    PBBX-128 #content feat: add controller and route scripts to list categoria via API

--- src/modules/scripts/reporteOptions.js
+++ src/modules/scripts/reporteOptions.js
@@ -1,12 +1,4 @@
 module.exports = {
-    categoriasValidas: [
-      'Iluminação Pública',
-      'Buraco na via',
-      'Vazamento de água',
-      'Entulho em local público',
-      'Semáforo quebrado',
-      'Outros'
-    ],
     statusValidos: [
       'Pendente',
       'Em andamento',
