483bbe8cb26a83b0edf68c96872d30e5e966b8bb
L0G1C06 <justgithub70@gmail.com>
2025-04-09 17:27:09 -0300

    PBBX-129 @content feat: added delete categoria endpoint

--- src/modules/categoria/categoria.routes.js
+++ src/modules/categoria/categoria.routes.js
@@ -54,4 +54,32 @@ router.post('/create', categoriaController.createCategoria);
  */
 router.get('/get', categoriaController.listCategorias);
 
+/**
+ * @swagger
+ * /api/categoria/{id}:
+ *   delete:
+ *     tags:
+ *       - Categoria
+ *     summary: Deleta uma categoria
+ *     description: Deleta uma categoria existente. Apenas usuários com papel de administrador (admin) têm permissão para deletar.
+ *     parameters:
+ *       - name: id
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Categoria deletada com sucesso
+ *       400:
+ *         description: ID da categoria não fornecido
+ *       401:
+ *         description: Token não fornecido ou inválido
+ *       403:
+ *         description: Acesso negado. Somente administradores podem deletar categorias
+ *       404:
+ *         description: Categoria não encontrada
+ *       500:
+ *         description: Erro interno do servidor
+ */
+router.delete('/:id', categoriaController.deleteCategoria);
+
 module.exports = router;
