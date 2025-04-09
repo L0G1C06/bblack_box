111744f5adadbe3713d3f2312e71d90aeca8b923
L0G1C06 <justgithub70@gmail.com>
2025-04-09 15:30:52 -0300

    PBBX-126 #content feat: added list of valid options for reporting category and status

---
+++ src/modules/scripts/reporteOptions.js
@@ -0,0 +1,16 @@
+module.exports = {
+    categoriasValidas: [
+      'Iluminação Pública',
+      'Buraco na via',
+      'Vazamento de água',
+      'Entulho em local público',
+      'Semáforo quebrado',
+      'Outros'
+    ],
+    statusValidos: [
+      'Pendente',
+      'Em andamento',
+      'Resolvido',
+      'Fechado sem solução'
+    ]
+  };
