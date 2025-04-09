3c41322bffa7f1e12b1e904567501a82b24373c2
L0G1C06 <justgithub70@gmail.com>
2025-04-09 10:39:18 -0300

    PBBX-121 #comment feat: add database model and migration to create the reporte forms into the database

---
+++ src/models/reporte.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const Reporte = sequelize.define('Reporte', {
+        /**
+         * Campos do banco de dados
+         * Foto do perfil nulo-> false
+         * Nome do perfil nulo-> false
+         * Horário do reporte nulo-> false
+         * Localização do reporte nulo-> false
+         * Descrição do reporte nulo-> false
+         * Imagem do reporte nulo-> false
+         * Avaliação do reporte (1 a 5) nulo-> true
+         */
+        fotoPerfil: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        nomePerfil: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        horarioReporte: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        localizacaoReporte: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        descricaoReporte: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        imagemReporte: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        avaliacaoReporte: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+                min: 0,
+                max: 5
+            }
+        }
+    }, {
+        tableName: 'reporte',
+        timestamps: true
+    });
+
+    return Reporte;
+};
